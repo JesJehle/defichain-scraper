d0c888cbc995b2e3a2098673443cea665a96188f
JesJehle <jesjehle@gmx.de>
2021-12-14 14:59:46 +0100

    sorted time

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ n_tokes = len(tokens)
 st.title("Lets make money with DefiChain")
 for token in tokens:
     df_filtered = df[df['name'] == token]
+    df_sorted = df_filtered.sort_values(by="time", key=pd.to_datetime)
     st.text(token)
-    fig = px.line(df_filtered, x='time', y='dex_diff')
+    fig = px.line(df_sorted, x='time', y='dex_diff')
     st.plotly_chart(fig, use_container_width=True)
